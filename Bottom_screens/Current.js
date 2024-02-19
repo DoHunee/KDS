568bffaefa936e5b73a21543e489df98b2c37a1e
장도훈 <jehgns1@gmail.com>
2024-02-19 10:02:58 +0900

    current.js 파일에도 소켓에 메세지 보내는 부분 추가

--- Bottom_screens/Current.js
+++ Bottom_screens/Current.js
@@ -1,37 +1,61 @@
 import { StyleSheet, View, Alert } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import OrderList from "../components/OrderList";
 import { useDispatch, useSelector } from "react-redux";
 import { onReady, onCancel } from "../store/storeSlice";
 import EmptyOrders from "../components/EmptyOrders";
 import Length from "../RightUpBar/Length";
+import { io } from "socket.io-client";
 
 const Current = ({ navigation }) => {
   const dispatch = useDispatch(); // Redux의 useDispatch를 사용하여 액션을 디스패치
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  const currentOrders = useSelector((state) => state.OrdersDistrubutionSclie.current); // Redux에서 상태를 가져오기 위해 useSelector를 사용
+  const currentOrders = useSelector(
+    (state) => state.OrdersDistrubutionSclie.current
+  ); // Redux에서 상태를 가져오기 위해 useSelector를 사용
   const [orders, setOrders] = useState([]); // 로컬 상태 orders를 사용하여 currentOrders를 업데이트
+  const socket = useRef(null);
+  const { stCode } = useSelector((state) => state.auth);
 
-  // 주문 상태를 업데이트하는 함수
-  const buttonPress = (data) => {
-    if (data.action === "준비완료") {
-      dispatch(onReady({ id: data.id }));
-    } else if (data.action === "주문취소") {
-      cancelOrder(data.id);
-    }
-  };
+  // 소켓
+  useEffect(() => {
+    // 컴포넌트 마운트 시 소켓 연결 생성
+    socket.current = io("http://211.54.171.41:8025/admin");
+
+    // 컴포넌트 언마운트 시 소켓 연결 종료
+    return () => {
+      if (socket.current) {
+        socket.current.disconnect();
+      }
+    };
+  }, []);
 
   // currentOrders가 업데이트될 때마다 orders를 업데이트
+  // 여기 24.02.19에 수정됐다!
   useEffect(() => {
     setOrders(currentOrders);
-  }, [orders, currentOrders]);
+  }, [currentOrders]); // 'currentOrders'의 변화에만 반응하도록 설정
 
   useEffect(() => {
     if (!isLoggedIn) {
       // Alert.alert("로그인 필요", "사용하기 전에 로그인이 필요합니다.");
     }
   }, [isLoggedIn]);
+
+  // 주문 상태를 업데이트하는 함수
+  const buttonPress = (data) => {
+    if (data.action === "준비완료") {
+      dispatch(onReady({ id: data.id }));
+      socket.current.emit("readyOrder", {
+        stCode: stCode,
+        id: data.id,
+        message: "고객님의 주문이 준비 완료되었습니다!",
+      });
+    } else if (data.action === "주문취소") {
+      cancelOrder(data.id);
+    }
+  };
 
   // 주문 취소 액션을 디스패치하고 사용자로부터 취소 이유를 선택받는 함수
   const cancelOrder = (orderId) => {
